ae6f7ec11ed9dcacda3be6f40c098fe9b876f4e5
jacky <1192964153@qq.com>
2020-09-27 11:05:59 +0800

    <no commit>

--- cloud_client_2/src/main/java/com/jacky/cloud_client/TestController.java
+++ cloud_client_2/src/main/java/com/jacky/cloud_client/TestController.java
@@ -19,4 +19,39 @@ public class TestController {
         return "book";
     }
 
+    public static class MyTask implements Runnable{
+
+        private String name;
+
+        public MyTask(String name){
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread " + name + " run-------------");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MyTask task1 = new MyTask("A");
+        MyTask task2 = new MyTask("B");
+        MyTask task3 = new MyTask("C");
+
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+        Thread thread3 = new Thread(task3);
+
+        thread1.start();
+        thread1.join();
+        thread2.start();
+        thread2.join();
+        thread3.start();
+    }
+
 }
--- configserver/src/test/java/com/jacky/configserver/ConfigserverApplicationTests.java
+++ configserver/src/test/java/com/jacky/configserver/ConfigserverApplicationTests.java
@@ -9,8 +9,35 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ConfigserverApplicationTests {
 
+    public class MyTask implements Runnable{
+
+        private String name;
+
+        public MyTask(String name){
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread" + name + "run-------------");
+        }
+    }
+
     @Test
     public void contextLoads() {
+        MyTask task1 = new MyTask("A");
+        MyTask task2 = new MyTask("B");
+        MyTask task3 = new MyTask("C");
+
+        new Thread(task1).start();
+        new Thread(task2).start();
+        new Thread(task3).start();
+
     }
 
 }
